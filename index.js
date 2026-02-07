ecafa67332d728fa5b712e7e16a79d436f75a01c
MansuraAlhassan <mansuraalhassan297@gmail.com>
2026-02-07 05:41:43 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,133 @@
+/*let firstName ="ABIE MANSURA";
+let lastName ="ALHASSAN";
+console.log(firstName);
+console.log(lastName);
+firstName ="Adams";
+console.log(firstName);
+
+
+//let age ="11";
+//console.log(age);
+
+let ages = 11;
+//console.log(ages);
+ages = 20;
+console.log(ages);
+
+
+// constant
+const pi = 3.14;
+console.log(pi);
+//pi = 10;
+//console.log(pi);
+
+let text1="Silicon Sqaud";
+        text1 +="Software Engenerring x Cyber Security";
+        document.getElementById("demo").innerHTML=text1;
+
+
+
+
+let text2="Silicon Sqaud";
+        text2 +="Software Engenerring x Cyber Security";
+        document.getElementById("demo1").innerHTML=text2;
+
+
+let x =50 * 30;
+let a = 20;
+let result = x+a;
+console.log (result);
+document.getElementById("result").innerHTML=x+a;
+
+console.log ("hello silicon");
+console.log ("this is just a text");
+window.alert ("welcome to silicon squad");
+
+document.getElementById("my h1").textContent="Hello silicon sqaud";
+document.getElementById("my p").textContent= "This paragraph was added using java script";
+
+let age = 25;
+console.log (`you are ${age} years old`);
+
+let price = 10.99;
+let gpa = 4.5;
+console.log(`The price is $10.99`);
+console.log(`your gpa is`)
+
+
+let firstName="Kamil";
+let favoriteFood="fufu";
+
+console .log (`my name is ${firstName}`);
+let email='kamail@gmail.com;' 
+console.log(`your email is ${email}`);
+
+let Online = false;
+console.log( typeof Online);
+console.log(`kamail is Online: ${Online}`);
+let forSale =true;
+let isStudent =true;
+console.log(`is this car forSale?: ${forSale}`);
+let fullName = "Mohammed Kamil";
+let age=25;
+let isStudent=true;
+*/
+
+/*document.getElementById("p1").textContent=fullName;
+document.getElementById("p2").textContent=age;
+
+document.getElementById("p1").textContent=`your name is ${fullName}`;
+
+let students =7;
+
+console.log(students);
+students = students + 1;
+console.log(students);
+students =students-1;
+console.log(students);
+students = students * 2;
+console.log(students);
+*/
+// let age = 23;
+// age = age + 5
+//or
+//let age = 23;
+//age +=5;
+//console.log(age);
+
+//let result = 1 + 2 *3 + 4 ** 2;
+//console.log(result);
+
+//how create inputs;
+//easy way;
+//window.prompt(`Enter your age`);
+
+//let age = window.prompt( `what is your age`);
+//console . log (age);
+
+//let age = window.prompt(`enter your age`);
+//console.log(age);
+
+
+//professional way
+
+let UserName;
+
+UserName = document.getElementById(`sub`).onclick = function(){
+        UserName = document.getElementById(`text`).value;
+        console.log(UserName);
+        document.getElementById(`myText`).textContent = `Hello ${UserName} `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
